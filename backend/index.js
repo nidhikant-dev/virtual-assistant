478ded1a9aabb3177d37d17fd73de062dcdd6ff7
nidhikant-dev <nidhikantsingh419@gmail.com>
2025-11-25 13:13:39 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import geminiResponse from "./gemini.js"
 
 const app=express()
 app.use(cors({
-    origin:"https://virtual-assistant-kslj.onrender.com",
+    origin:"virtual-assistant-h8cf.vercel.app",
     credentials:true
 }))
 const port=process.env.PORT || 5000
